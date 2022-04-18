779686b00993f318a7dbc69c699c52c4eba1914a
nursolih <93719688+nursolih@users.noreply.github.com>
2022-04-18 09:10:11 +0700

    push jawaban uts

---
+++ pacman.java
@@ -0,0 +1,18 @@
+package pacman;
+import javax.swing.JFrame;
+
+public class pacman extends JFrame{
+    public pacman(){
+        add(new Model());
+    }
+
+    public static void main(String[] args) {
+        pacman pac = new pacman();
+        pac.setVisible(true);
+        pac.setTitle("pacman");
+        pac.setSize(380,420);
+
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
+    }
+}
